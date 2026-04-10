acba0b510447fd371cb58372a0d4aff5f5482a73
Matias Heiberg Nielsen <perm_diners7z@icloud.com>
2026-04-10 13:05:30 +0200

    Oprettet Points klasse med logik fra main

---
+++ TeknologiProjekt/Points.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeknologiProjekt
+{
+    public static class Points
+    {
+        private static int _points = 0;
+        private static readonly object _lock = new object();
+
+        public static void AddPoints()
+        {
+            lock (_lock)
+            {
+                _points++;
+            }
+        }
+        public static int GetPoints()
+        {
+            return _points;
+        }
+    }
+}
